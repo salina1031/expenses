3fdd13350c2d4d93c79b793ecc3724825c3b38f1
salina1031 <salinabhattarai09@gmail.com>
2026-01-18 15:30:31 +0545

    fix the spelling

--- script.js
+++ script.js
@@ -2,14 +2,16 @@
 expensesName=document.getElementById("ExpensesName");
 amount=document.getElementById("amount");
 addButton=document.querySelector("button");
-List=document.getElementsByClassName("listConatiner")
+List=document.querySelector(".listContainer")
 Total=document.querySelector("span")
 //loading the expenses form the local storage or creating the array if no expenses  exists
-Expenses=JSON.parse(localStorage.getItem("Expenses"))||[]
+ let Expenses=JSON.parse(localStorage.getItem("Expenses"))||[]
 //save and render the data 
 function saveRender(){
-    localStorage.setItem("expenses",JSON.stringify(expenses))
+    localStorage.setItem("Expenses", JSON.stringify(Expenses));
+    renderExpenses();
 }
+
 
 //randers /show the expenses when web page is open/load
 renderExpenses();//this is the function to call the elements/ content in the function
@@ -17,8 +19,8 @@ renderExpenses();//this is the function to call the elements/ content in the fun
 //for adding the expenses
 addButton.addEventListener("click",()=>{
     const expName=expensesName.value;
-    const Amt=number(amount.value)
-    if (expName=""||amt<=0)
+    const Amt = parseFloat(amount.value);
+    if (expName==""||Amt<=0)
         return;
     //keeping all the value in the expense array which contain(date,expName,Amt)
     const expense={
@@ -27,7 +29,7 @@ addButton.addEventListener("click",()=>{
         Amt
     };
     //add expense to the expenses
-    expenses.push(expense)
+    Expenses.push(expense)
     //calling the function saveRender to store in the web local storage
     saveRender();
     
@@ -35,29 +37,27 @@ addButton.addEventListener("click",()=>{
     amount.value=""
 });
 
-
 //for rendering the expenses n the web page so creating the function
 //which is called in above code
 function renderExpenses(){
     List.innerHTML="";
     let total=0;//initial total amount
-
-    expenses.foreach(exp=>{
+    Expenses.forEach(exp=>{
         total+=exp.Amt
     
 
     //creating the div to display the amount
     const div=document.createElement("div");
     //writing in the web so it can be display in the page
-    div.innerHTML=`${exp.expName} -RS.${exp.Amt}
-    <button onclick="deleteExp(${exp.id})">Delete</button>`//creating the delete button
-    List.appendChild(div)
+    div.innerHTML=`${exp.expName}: RS.${exp.Amt}
+    <button class="button" onclick="deleteExp(${exp.id})">Delete</button>`;//creating the delete button
+    List.appendChild(div);
     });
     Total.textContent=`Total Expenses: RS${total}`
 
 }
 //delete the expenses
-function deleteExp(){
-    expenses.expenses.filter(ex=>ex.id!=id)
+function deleteExp(id) {
+    Expenses = Expenses.filter(exp => exp.id !== id);
     saveRender();
 }
